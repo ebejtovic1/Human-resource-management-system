54de6008ec73d1798083cd4d479973d31defced9
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2020-07-13 12:20:48 +0200

    initial commit

---
+++ ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+    }
+}
