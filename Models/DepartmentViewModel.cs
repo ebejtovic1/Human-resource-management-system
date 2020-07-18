27a7f6c543fce6c56cc6121fd486411e32c4515e
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2020-07-18 15:22:17 +0200

    Dodan model Department

---
+++ Models/DepartmentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMSCrypto.Models
+{
+    public class DepartmentViewModel
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int LocationId { get; set; }
+        public virtual Location Location { get; set; }
+    }
+}
