ea3727a84a0bbb1e63e6c9eda9c3a52f660708e1
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2020-07-18 15:17:02 +0200

    Dodan Job Model

---
+++ Models/JobViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMSCrypto.Models
+{
+    public class JobViewModel
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
