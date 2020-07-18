4729bd1914b40cda0921cb125dd0fb504e8b1152
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2020-07-18 15:12:09 +0200

    Dodani modeli za zaposlenike i lokacije

---
+++ Models/EmployeeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMSCrypto.Models
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set;  }
+        public DateTime DateOfBirth { get; set; }
+        public String Address { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+        public String Email { get; set; }
+        public Double Salary { get; set; }
+        public String BrojRacuna { get; set; }
+        //job
+        //department
+
+    }
+}
---
+++ Models/LocationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace HRMSCrypto.Models
+{
+    public class LocationViewModel
+    {
+        public int Id { get; set;}
+        public String Address { get; set; }
+        public String City { get; set; }
+
+    }
+}
